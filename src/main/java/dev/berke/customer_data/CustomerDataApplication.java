c8340c543d66890a3bc0f843af5f955f103875b3
Berke Sayın <sayinberke34@gmail.com>
2025-07-04 19:40:47 +0300

    first commit

---
+++ src/main/java/dev/berke/customer_data/CustomerDataApplication.java
@@ -0,0 +1,13 @@
+package dev.berke.customer_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/berke/customer_data/CustomerDataApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.berke.customer_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
